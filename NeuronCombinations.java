6e601a983a7fc934f6fee72c664d6dc06b9d15a0
Abdullah Almetwally <aalmetwally38@gmail.com>
2024-03-07 22:25:23 -0500

    pushing to aBranch

--- NeuronCombinations.java
+++ NeuronCombinations.java
@@ -18,6 +18,7 @@ public class NeuronCombinations {
                 // {9, 3, 1}
         };
         System.out.println("commit");
+        System.out.println("pushing to aBranch");
 
         for (int[] aCase : cases) {
             int n = aCase[0];
